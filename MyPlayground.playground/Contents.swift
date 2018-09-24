b7d783a8a7a789026b6408561829429757d218ee
SAaronB <programmingsean@gmail.com>
2018-09-24 09:16:13 -0600

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var int = 35
+var notInt = 34
+
+var name = readLine()
+let theName = name
+
+print(str)
+
